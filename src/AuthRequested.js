80b8fca15d6433a765eb6ed4ebc68d69ce7c0f9b
Metál Ádám <devmetal91@gmail.com>
2017-03-06 17:56:17 +0100

    formatting

--- src/AuthRequested.js
+++ src/AuthRequested.js
@@ -13,40 +13,40 @@ function AuthRequested<T: *>(
   ComposedComponent: ReactClass < T & InjectedProps >,
 ): ReactClass < T > {
   class AuthRequestedComponent extends Component {
-  componentDidMount() {
-    this.props.listen();
-  }
-
-  render() {
-    const { auth, loading, logout, login, ...rem} = this.props;
-    if (loading === true) {
-      return null;
+    componentDidMount() {
+      this.props.listen();
     }
 
-    return (
-      <ComposedComponent
-        {...rem}
-        auth={auth}
-        logout={logout}
-        login={login}
-      />
-    )
+    render() {
+      const { auth, loading, logout, login, ...rem} = this.props;
+      if (loading === true) {
+        return null;
+      }
+
+      return (
+        <ComposedComponent
+          {...rem}
+          auth={auth}
+          logout={logout}
+          login={login}
+        />
+      )
+    }
   }
-}
 
-const mapStateToProps = ({ auth }) => ({
-  auth: auth.auth,
-  user: auth.user,
-  loading: auth.loading,
-});
+  const mapStateToProps = ({ auth }) => ({
+    auth: auth.auth,
+    user: auth.user,
+    loading: auth.loading,
+  });
 
-const mapDispatchToProps = (dispatch) => ({
-  logout() { dispatch(Actions.logout()) },
-  login() { dispatch(Actions.startAuth()) },
-  listen() { dispatch(Actions.startListenToAuth()) },
-});
+  const mapDispatchToProps = (dispatch) => ({
+    logout() { dispatch(Actions.logout()) },
+    login() { dispatch(Actions.startAuth()) },
+    listen() { dispatch(Actions.startListenToAuth()) },
+  });
 
-return connect(mapStateToProps, mapDispatchToProps)(AuthRequestedComponent);
+  return connect(mapStateToProps, mapDispatchToProps)(AuthRequestedComponent);
 }
 
 export default AuthRequested;
